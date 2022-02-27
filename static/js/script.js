163a5984ddbe6a5ac44004a683480c9b0ea504af
Wesley Dias <wesd_444@hotmail.com>
2022-02-27 16:51:33 -0300

    add: tooltips

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,27 @@
 let currentValue = 0;
 
 function animateValue(obj, start, end, duration) {
-    let startTimestamp = null;
-    const step = (timestamp) => {
-      if (!startTimestamp) startTimestamp = timestamp;
-      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-      currentValue = progress * (end - start) + start
-      obj.innerText = `${currentValue.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`
-      if (progress < 1) {
-        window.requestAnimationFrame(step);
-      }
-    };
-    window.requestAnimationFrame(step);
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    currentValue = progress * (end - start) + start;
+    obj.innerText = `${currentValue.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
 }
 
-function updateDonatedValue() {
-    fetch('/api/v1/donated')
-    .then((resp) => resp.json())
-    .then((data) => {
-        const newValue = data.donated_usd;
-        animateValue(document.getElementById("donated"), currentValue, newValue, 10000);
-    });
+function updateDonatedValue(isFirst) {
+  fetch('/api/v1/donated')
+  .then((resp) => resp.json())
+  .then((data) => {
+    const newValue = data.donated_usd;
+    animateValue(document.getElementById('donated'), currentValue, newValue, isFirst ? 1500 : 55000);
+  });
 };
 
-updateDonatedValue();
+updateDonatedValue(true);
 setInterval(updateDonatedValue, 60000);
