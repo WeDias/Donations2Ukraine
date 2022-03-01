cb5e16ca327e1786a1a2a11af3e066ed6c0afb99
Wesley Dias <wesd_444@hotmail.com>
2022-03-01 13:40:15 -0300

    add: polkadot coin and threads

--- services/donations_all.py
+++ services/donations_all.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 from .donations import Donations
 from .donations_bitcoin import DonationsBitcoin
 from .donations_ethereum import DonationsEthereum
@@ -13,7 +14,14 @@ class DonationsAll(Donations):
     donations_polkadot = DonationsPolkadot()
 
     def get_value_in_dollars(self) -> float:
-        return self.donations_bitcoin.get_value_in_dollars() + \
-            self.donations_ethereum.get_value_in_dollars() + \
-            self.donations_usdt.get_value_in_dollars() + \
-            self.donations_polkadot.get_value_in_dollars()
+        value_in_dollars = 0
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.donations_bitcoin.get_value_in_dollars),
+                executor.submit(self.donations_ethereum.get_value_in_dollars),
+                executor.submit(self.donations_usdt.get_value_in_dollars),
+                executor.submit(self.donations_polkadot.get_value_in_dollars)
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                value_in_dollars += future.result()
+        return value_in_dollars
--- services/donations_polkadot.py
+++ services/donations_polkadot.py
@@ -1,10 +1,20 @@
+from csv import DictReader
+import requests
 from .donations import Donations
 
 
 class DonationsPolkadot(Donations):
 
-    BASE_URL = ''
+    BASE_URL = 'https://api.dotscanner.com'
     POLKADOT_ADDRESS = '1x8aa2N2Ar9SQweJv9vsuZn3WYDHu7gMQu1RePjZuBe33Hv'
 
     def get_value_in_dollars(self) -> float:
-        return 0 # TODO: get total value of donations in polkadot
+        response = requests.get(f'{self.BASE_URL}/stats/prices/dot').json()
+        polkadot_price = response['data']['price']
+
+        value_in_dollars = 0
+        response = requests.get(f'{self.BASE_URL}/accounts/{self.POLKADOT_ADDRESS}/transfers_csv?chain=Polkadot')
+        for transaction in DictReader(response.text.split('\n')):
+            if transaction['To'] == self.POLKADOT_ADDRESS:
+                value_in_dollars += float(transaction['Amount']) * polkadot_price
+        return value_in_dollars
